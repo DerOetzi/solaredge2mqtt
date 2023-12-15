784d32ed6779f37ab0c9903fc9323c50bffe69fa
Johannes Ott <mail@johannes-ott.net>
2023-12-15 22:51:03 +0100

    Read basic data for meters and batteries
    Signed-off-by: Johannes Ott <mail@johannes-ott.net>

--- solaredge2mqtt/models/sunspec.py
+++ solaredge2mqtt/models/sunspec.py
@@ -1,8 +1,12 @@
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from pydantic import BaseModel
-
-from solaredge_modbus import C_SUNSPEC_DID_MAP, sunspecDID, INVERTER_STATUS_MAP
+from solaredge_modbus import (
+    BATTERY_STATUS_MAP,
+    C_SUNSPEC_DID_MAP,
+    INVERTER_STATUS_MAP,
+    sunspecDID,
+)
 
 
 class SunSpecInfo(BaseModel):
@@ -31,66 +35,70 @@ class SunSpecInfo(BaseModel):
 
 class SunSpecBaseValue(BaseModel):
     @staticmethod
-    def scale_value(value: int, scale: int, digits: int = 2) -> float:
+    def scale_value(
+        data: Dict[str, str | int],
+        value_key: str,
+        scale_key: Optional[str] = None,
+        digits: Optional[int] = 2,
+    ) -> float:
+        if scale_key is None:
+            scale_key = f"{value_key}_scale"
+
+        value = int(data[value_key])
+        scale = int(data[scale_key])
+
         return round(value * 10**scale, digits)
 
     @staticmethod
     def is_three_phase(data: Dict[str, Any]) -> bool:
-        return data["c_sunspec_did"] is sunspecDID.THREE_PHASE_INVERTER.value
+        return data["c_sunspec_did"] in [
+            sunspecDID.THREE_PHASE_INVERTER.value,
+            sunspecDID.WYE_THREE_PHASE_METER.value,
+            sunspecDID.DELTA_THREE_PHASE_METER.value,
+        ]
 
 
 class SunSpecACCurrent(SunSpecBaseValue):
     current: float
     l1_current: float
-    l2_current: float | None
-    l3_current: float | None
+    l2_current: Optional[float] = None
+    l3_current: Optional[float] = None
 
     @classmethod
     def map(cls, data: Dict[str, str | int]) -> Dict[str, float]:
         values = {
-            "current": cls.scale_value(data["current"], data["current_scale"]),
-            "l1_current": cls.scale_value(data["l1_current"], data["current_scale"]),
+            "current": cls.scale_value(data, "current"),
+            "l1_current": cls.scale_value(data, "l1_current", "current_scale"),
         }
 
         if cls.is_three_phase(data):
-            values["l2_current"] = cls.scale_value(
-                data["l2_current"], data["current_scale"]
-            )
-            values["l3_current"] = cls.scale_value(
-                data["l3_current"], data["current_scale"]
-            )
+            values["l2_current"] = cls.scale_value(data, "l2_current", "current_scale")
+            values["l3_current"] = cls.scale_value(data, "l1_current", "current_scale")
 
         return values
 
 
 class SunSpecACVoltage(SunSpecBaseValue):
-    l1_voltage: float
-    l2_voltage: float | None
-    l3_voltage: float | None
-    l1n_voltage: float
-    l2n_voltage: float | None
-    l3n_voltage: float | None
+    l1_voltage: Optional[float] = None
+    l2_voltage: Optional[float] = None
+    l3_voltage: Optional[float] = None
+    l1n_voltage: Optional[float] = None
+    l2n_voltage: Optional[float] = None
+    l3n_voltage: Optional[float] = None
 
     @classmethod
     def map(cls, data: Dict[str, str | int]) -> Dict[str, float]:
-        values = {
-            "l1_voltage": cls.scale_value(data["l1_voltage"], data["voltage_scale"]),
-            "l1n_voltage": cls.scale_value(data["l1n_voltage"], data["voltage_scale"]),
-        }
+        values = {}
+        for phase in ["l1", "l2", "l3"]:
+            if f"{phase}_voltage" in data:
+                values[f"{phase}_voltage"] = cls.scale_value(
+                    data, f"{phase}_voltage", "voltage_scale"
+                )
 
-        if cls.is_three_phase(data):
-            values["l2_voltage"] = cls.scale_value(
-                data["l2_voltage"], data["voltage_scale"]
-            )
-            values["l2n_voltage"] = cls.scale_value(
-                data["l2n_voltage"], data["voltage_scale"]
-            )
-            values["l3_voltage"] = cls.scale_value(
-                data["l3_voltage"], data["voltage_scale"]
-            )
-            values["l3n_voltage"] = cls.scale_value(
-                data["l3n_voltage"], data["voltage_scale"]
-            )
+            if f"{phase}n_voltage" in data:
+                values[f"{phase}n_voltage"] = cls.scale_value(
+                    data, f"{phase}n_voltage", "voltage_scale"
+                )
 
         return values
 
@@ -102,18 +110,12 @@ class SunSpecACPower(SunSpecBaseValue):
     power_factor: float
 
     @classmethod
-    def map(cls, data: Dict[str, str | int]) -> Dict[str, Any]:
+    def map(cls, data: Dict[str, str | int], power_key: str) -> Dict[str, Any]:
         return {
-            "power": cls.scale_value(data["power_ac"], data["power_ac_scale"]),
-            "power_reactive": cls.scale_value(
-                data["power_reactive"], data["power_reactive_scale"]
-            ),
-            "power_apparent": cls.scale_value(
-                data["power_apparent"], data["power_apparent_scale"]
-            ),
-            "power_factor": cls.scale_value(
-                data["power_factor"], data["power_factor_scale"]
-            ),
+            "power": cls.scale_value(data, power_key),
+            "power_reactive": cls.scale_value(data, "power_reactive"),
+            "power_apparent": cls.scale_value(data, "power_apparent"),
+            "power_factor": cls.scale_value(data, "power_factor"),
         }
 
 
@@ -128,8 +130,8 @@ class SunSpecAC(SunSpecBaseValue):
         return {
             "current": SunSpecACCurrent.map(data),
             "voltage": SunSpecACVoltage.map(data),
-            "power": SunSpecACPower.map(data),
-            "frequency": cls.scale_value(data["frequency"], data["frequency_scale"]),
+            "power": SunSpecACPower.map(data, "power_ac"),
+            "frequency": cls.scale_value(data, "frequency"),
         }
 
 
@@ -141,40 +143,78 @@ class SunSpecDC(SunSpecBaseValue):
     @classmethod
     def map(cls, data: Dict[str, str | int]) -> Dict[str, Any]:
         return {
-            "current": cls.scale_value(data["current_dc"], data["current_dc_scale"]),
-            "voltage": cls.scale_value(data["voltage_dc"], data["voltage_dc_scale"]),
-            "power": cls.scale_value(data["power_dc"], data["power_dc_scale"]),
+            "current": cls.scale_value(data, "current_dc"),
+            "voltage": cls.scale_value(data, "voltage_dc"),
+            "power": cls.scale_value(data, "power_dc"),
         }
 
 
-class SunSpecInverter(BaseModel):
+class SunSpecInverter(SunSpecBaseValue):
     info: SunSpecInfo
     ac: SunSpecAC
     dc: SunSpecDC
+    energy_total: float
     status: str
 
     def __init__(self, data: Dict[str, str | int]):
         info = SunSpecInfo.map(data)
         ac = SunSpecAC.map(data)
         dc = SunSpecDC.map(data)
+        energy_total = self.scale_value(data, "energy_total")
         status = INVERTER_STATUS_MAP[data["status"]]
 
-        super().__init__(info=info, ac=ac, dc=dc, status=status)
+        super().__init__(
+            info=info, ac=ac, dc=dc, energy_total=energy_total, status=status
+        )
 
 
-class SunSpecMeter(BaseModel):
+class SunSpecMeter(SunSpecBaseValue):
     info: SunSpecInfo
+    current: SunSpecACCurrent
+    voltage: SunSpecACVoltage
+    power: SunSpecACPower
+    frequency: float
 
     def __init__(self, data: Dict[str, str | int]):
         info = SunSpecInfo.map(data)
+        current = SunSpecACCurrent.map(data)
+        voltage = SunSpecACVoltage.map(data)
+        power = SunSpecACPower.map(data, "power")
+        frequency = self.scale_value(data, "frequency")
 
-        super().__init__(info=info)
+        super().__init__(
+            info=info,
+            current=current,
+            voltage=voltage,
+            power=power,
+            frequency=frequency,
+        )
 
 
 class SunSpecBattery(BaseModel):
     info: SunSpecInfo
+    status: str
+    current: float
+    voltage: float
+    power: float
+    state_of_charge: float
+    state_of_health: float
 
     def __init__(self, data: Dict[str, str | int]):
         info = SunSpecInfo.map(data)
+        status = BATTERY_STATUS_MAP[data["status"]]
+        current = round(data["instantaneous_current"], 2)
+        voltage = round(data["instantaneous_voltage"], 2)
+        power = round(data["instantaneous_power"], 2)
+        state_of_charge = round(data["soe"], 2)
+        state_of_health = round(data["soh"], 2)
 
-        super().__init__(info=info)
+        super().__init__(
+            info=info,
+            status=status,
+            current=current,
+            voltage=voltage,
+            power=power,
+            state_of_charge=state_of_charge,
+            state_of_health=state_of_health,
+        )
